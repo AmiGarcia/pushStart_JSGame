5d002abaa3037fa82741443038c715fe9ae0a702
AmiHoppus182 <aiami@live.com>
2019-01-15 17:56:49 -0200

    creating character from json setup

--- js/main.js
+++ js/main.js
@@ -15,15 +15,18 @@ let Loader = PIXI.loader;
 let Container = PIXI.Container;
 
 var stage = new Container();
+var character = new PIXI.Graphics();
+var levels;
 
 Loader
   .add('levels', 'json/levels.json')
-  .load(setup);
+  .load(Setup);
 
 
-function setup()
+function Setup()
 {
-  var levels = Loader.resources.levels.data;
+  //load levels from our loader
+  levels = Loader.resources.levels.data;
   console.log(JSON.stringify(levels[0]));
   console.log(JSON.stringify(levels[1]));
   var level01 = levels[0];
@@ -31,7 +34,33 @@ function setup()
   console.log(levels.length);
 
 
-  renderer.renderer(stage);
+  
+  SetupLevel(1);
+
+  renderer.render(stage);
+  
+}
+
+function SetupLevel(level)
+{
+  //Add one to get right level (we dont want levels begining with 0)
+  var currentLevel = levels[level + 1];
+  //initial color of our character
+  var initialColor = currentLevel["initial"]["color"]; 
+  //initial size of our character. Multiplied by 20 cause 1 it's too small for screen
+  var initialSize = currentLevel["initial"]["size"] * 20; 
+  //Now we can create our character for this level
+  CreateCharacter(initialColor, initialSize);
+
+}
+
+function CreateCharacter(initialColor, initialSize)
+{
+  character.beginFill(initialColor);
+  var y = renderer.width / 2;
+  character.drawRect(10, y, initialSize, initialSize);
+  //After this little setup, we can add it to stage and show it on screen
+  stage.addChild(character);
 }
 
 //load an image and run the `setup` function when it's done
